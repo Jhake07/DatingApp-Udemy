da5ee41bd03b8f11a6ec972576cf4d329e6a4fe7
jhakeumali@gmail.com <jhakeumali@gmail.com>
2025-01-13 20:44:58 +0800

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +24,9 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
