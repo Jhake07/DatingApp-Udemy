b36dc93b826281d76ec3b2c504546e19bfc8e6f8
jhakeumali@gmail.com <jhakeumali@gmail.com>
2025-02-14 23:43:24 +0800

    End of Section 13

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -5,11 +5,19 @@ namespace API.Extensions;
 
 public static class ClaimsPrincipleExtensions
 {
-     public static string GetUsername(this ClaimsPrincipal user)
+    public static string GetUsername(this ClaimsPrincipal user)
     {
-        var username = user.FindFirstValue(ClaimTypes.NameIdentifier) 
+        var username = user.FindFirstValue(ClaimTypes.Name) 
             ?? throw new Exception("Cannot get username from token");
         
         return username;
+    }
+
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userid =int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) 
+            ?? throw new Exception("Cannot get userid from token"));
+        
+        return userid;
     }
 }
